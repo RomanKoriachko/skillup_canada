1daa7412c41efe1cde7eda4a7e51b6b66f933881
Roman <mazaxaka.tyt@gmail.com>
2023-06-28 15:41:24 +0300

    add certificate-section

--- app.js
+++ app.js
@@ -110,45 +110,7 @@ window.addEventListener("scroll", function () {
 });
 
 // Slider
-// $(document).ready(function () {
-//   $(".slider").slick({
-//     arrows: false,
-//     infinite: true,
-//     dots: false,
-//     variableWidth: true,
-//     slidesToShow: 3,
-//     centerMode: true,
-//     centerPadding: "60px",
-//     responsive: [
-//       {
-//         breakpoint: 576,
-//         settings: {
-//           centerMode: false,
-//         },
-//       },
-//     ],
-//   });
-// });
 
-$(document).ready(function () {
-    $(".slider-video").slick({
-        arrows: false,
-        infinite: true,
-        dots: false,
-        variableWidth: true,
-        slidesToShow: 3,
-        centerMode: true,
-        centerPadding: "60px",
-        responsive: [
-            {
-                breakpoint: 576,
-                settings: {
-                    centerMode: false,
-                },
-            },
-        ],
-    });
-});
 $(document).ready(function () {
     $(".slider-text").slick({
         arrows: false,
